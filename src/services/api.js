c428a47393596b413f03d0eafe2530a129952df3
Pepe MR <pepemartos98@gmail.com>
2026-04-08 12:10:30 +0200

    deploy front

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/transacciones";
+const API_URL = `${import.meta.env.VITE_API_URL}/transacciones`;
 
 export const obtenerTransacciones = async () => {
   const res = await fetch(API_URL);
